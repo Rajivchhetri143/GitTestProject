ab9521d2e0c767a3f8dfa1b6f6800edcb9ca7b32
Rajivchhetri143 <125329404+Rajivchhetri143@users.noreply.github.com>
2023-10-13 20:12:27 -0400

    Added comments about branches

--- src/com/coderscampus/domain/profile.java
+++ src/com/coderscampus/domain/profile.java
@@ -1,26 +1,37 @@
  package com.coderscampus.domain;
 
+ import java.time.LocalDate;
+ 
+ 
+ /**
+  * Branches
+  * main/master -> represents the code that"s actually live on the internet (ie coderscampus.com)
+  * UAT
+  *QA
+  *development
+  *
+  * 
+  * Environments
+  * Production -> main/master  
+  *UAT (not typical)
+  *QA
+  *Dev
+  */
  public class profile {
   private Integer id;
-  private String name;
-  private String url;
-
-  public Integer getId() {
-	return id;
-  }
-  public void setId(Integer id) {
+  private String profileName;
+  private LocalDate getcreatedDate;
+ 
+  public LocalDate getCreatedDate() {
+	  return createdDate;
+}
+public  void setId(Integer id) {
 	this.id = id;
+}
+public String getProfileName() {
+	 return profileName;
   }
-  public String getName() {
-	return name;
-  }
-  public void setName(String name) {
-	this.name = name;
-  }
-  public String getUrl() {
-	return url;
-  }
-  public void setUrl(String url) {
-	this.url = url;
+  public void setProfileNameStringName(String name) {
+   this.profileName = name;
   }
 }
